4325367472005b62a585655eefa4d28cb0444330
Zachary Darneille <zakdarn@gmail.com>
2020-10-03 02:46:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+      end
+
+  def match(word)
+    matches = []
+    word.each do |x|
+     if @word == x
+       matches << x
+     else
+       matches
+       end
+    end
+  end
+
+end
+
+  
+  
+
